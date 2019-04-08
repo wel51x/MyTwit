0f25c4f3fcde31856a7e3f0b7d2edb002ff6155b
Winston <wxl51@yahoo.com>
2019-04-08 13:11:36 -0500

    added DB model

--- mytwit/app.py
+++ mytwit/app.py
@@ -1,8 +1,12 @@
+"""Main appl and routing logic for MyTwit."""
 from flask import Flask
+from .models import DB
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():
