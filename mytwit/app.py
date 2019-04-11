d2a40d702aaec16b53a4eeffd080b02c16cff268
Winston <wxl51@yahoo.com>
2019-04-10 22:43:19 -0500

    Day3 - full blown app

--- mytwit/app.py
+++ mytwit/app.py
@@ -1,6 +1,9 @@
 """Main appl and routing logic for MyTwit."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
+from .twitter import update_all_users
+from .twitter import add_or_update_user
 from os import getenv
 from dotenv import load_dotenv
 
@@ -18,10 +21,40 @@ def create_app():
     def root():
         return render_template('base.html', title='Home', users=User.query.all())
 
-    @app.route('/user/<name>')
-    def user(name):
-        tweets = User.query.filter(User.name == name).first().tweets
-        return render_template('user.html', title=name, tweets=tweets)
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to self, bozo!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
+    @app.route('/update')
+    def update():
+        update_all_users()
+        message = 'All tweets updated!'
+        return render_template('base.html', title='Home',
+                                users=User.query.all(), message=message)
 
     @app.route('/reset')
     def reset():
@@ -39,11 +72,17 @@ def create_app():
         uzerz =[]
         tweetz = []
         for user in users:
-        	uzerz.append(user.name)
-        	tweets = User.query.filter(User.name==user.name).first().tweets
-        	for tweet in tweets:
-	        	tweetz.append(tweet.text)
+            uzerz.append(user.name)
+            tweets = User.query.filter(User.name==user.name).first().tweets
+            for tweet in tweets:
+                tweetz.append(tweet.text)
         
         return str(uzerz) + str(tweetz)
 
     return app
+'''
+    @app.route('/user/<name>')
+    def user(name):
+        tweets = User.query.filter(User.name == name).first().tweets
+        return render_template('user.html', title=name, tweets=tweets)
+'''
